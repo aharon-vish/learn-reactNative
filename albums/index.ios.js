5858c82dcac1fc4e3d78fe7ba7298e7b3a021ca2
AharonVishinsky <aharon.vish@gmail.com>
2017-04-17 11:16:00 +0300

    add album list component

--- albums/index.ios.js
+++ albums/index.ios.js
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {
     AppRegistry,
     StyleSheet,
     Text,
     View
 } from 'react-native';
-import Header from './src/components/header';
+import Header from './src/components/Header';
+import AlbumList from './src/components/AlbumList';
 
 const App = () => (
-    <Header headerText={'Album!'}/>
+    <View>
+        <Header headerText={'Album!'}/>
+        <AlbumList/>
+    </View>
 );
 
 AppRegistry.registerComponent('albums', () => App);
---
+++ albums/src/components/AlbumList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class AlbumList extends Component {
+    render() {
+        return (
+            <View>
+                <Text>album list</Text>
+            </View>
+        );
+    }
+}
+
+export default AlbumList ;
