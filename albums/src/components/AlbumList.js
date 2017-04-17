d5194b1195777d1f253b470b00cb26e414fdaf7d
aharon-vish <aharon.vish@gmail.com>
2017-04-17 20:44:55 +0300

    fetch data and display it albumList component

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,11 +1,29 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {albums: []};
+    }
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums').
+        then(response => this.setState({albums: response.data}))
+    }
+
+    renderAlbums (){
+        return this.state.albums.map(album=>
+        <Text key={album.title}>{album.title}</Text>
+        );
+    }
+
     render() {
         return (
             <View>
-                <Text>album list</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
