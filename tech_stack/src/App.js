b13c00f8f3c35e986ce69f2a5ece6bfbc1676351
aharon-vish <aharon.vish@gmail.com>
2017-04-26 21:53:54 +0300

    set a reducers

---
+++ tech_stack/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View} from 'react-native';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducers from './reducers';
+
+const App = () => {
+    return (
+        <Provider store{createStore(reducers)}>
+            <View/>
+        </Provider>
+
+    )
+};
+export default App ;
