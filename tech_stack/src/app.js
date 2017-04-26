c437de45ab3378c6817828962f9c6fa1280bbef4
aharon-vish <aharon.vish@gmail.com>
2017-04-26 21:28:04 +0300

    add new proj

---
+++ tech_stack/src/app.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+import {View} from 'react-native';
+
+const App = () => {
+    return (
+        <View>
+  kokoi
+        </View>
+    )
+};
+export default App ;
