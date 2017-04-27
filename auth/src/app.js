0d5c74e07caf09633f36d4d8c1ba5b0a1c74e2fe
AharonVishinsky <aharon.vish@gmail.com>
2017-04-27 09:16:24 +0300

    finish login app firebase

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,14 @@
 import firebase from 'firebase';
 import React, { Component} from 'react';
 import {View} from 'react-native';
-import { Header } from './components/common';
+import { Header,Button,CardSection,Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
-    componentWillMount(){
+    state = {loggedIn: null};
+
+    componentWillMount() {
         firebase.initializeApp({
             apiKey: 'AIzaSyCaSu6aDWh9P_CunQ2Nbj4EzW5QpG3gTU8',
             authDomain: 'authentication-76b53.firebaseapp.com',
@@ -15,16 +17,47 @@ class App extends Component {
             storageBucket: 'authentication-76b53.appspot.com',
             messagingSenderId: '653455678656'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({loggedIn: true});
+            } else {
+                this.setState({loggedIn: false});
+            }
+        });
+
     }
 
-    render(){
+    renderContent() {
+
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <CardSection>
+                        <Button onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                        </Button>
+                    </CardSection>
+                );
+            case false:
+                return <LoginForm/>;
+            default:
+                return (<View style={{flex: 1}}><Spinner size="large"/></View>);
+        }
+    }
+
+    render() {
         return (
-          <View>
-              <Header headerText="Authentication"/>
-              <LoginForm/>
-          </View>
+            <View style={{flex: 1}}>
+                <Header headerText="Authentication"/>
+                {this.renderContent()}
+            </View>
         );
     }
 }
-
+const styles = {
+    content: {
+        padding: 100
+    }
+};
 export default App ;
