8d58a5c5ea796e4565724facb417e656aa934d75
AharonVishinsky <aharon.vish@gmail.com>
2017-05-07 19:11:17 +0300

    add edit employee

--- manager/src/Router.js
+++ manager/src/Router.js
@@ -3,6 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
+import EmployeeEdit from './components/EmployeeEdit';
 const RouterComponent = ()=> {
     return (
         <Router sceneStyle={{paddingTop:65}}>
@@ -19,6 +20,7 @@ const RouterComponent = ()=> {
                     initial/>
             </Scene>
             <Scene key="employeeCreate" component={EmployeeCreate} title="Employee Create"/>
+            <Scene key="employeeEdit" component={EmployeeEdit} title="Employee Edit"/>
         </Router>
     );
 };
